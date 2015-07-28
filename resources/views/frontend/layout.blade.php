4fe9ff4e37b89c2fc896d5c0301d90a49fd23e3c
Dmitry Groza <boxfrommars@gmail.com>
2015-07-28 05:53:10 +0300

    mobile content-table

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -6,14 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="yvzIVHZghvLLFJArEmBKcr5HGABsieiNZYLausg9Loo" />
     <meta name='yandex-verification' content='74f87e2ca2368e81' />
-    {{--<link rel="stylesheet" type="text/css" href="/frontend/style.css" media="screen" />--}}
 
-    <!-- build:css(public) css/all.css -->
-    <!-- bower:css -->
-    <link rel="stylesheet" href="/bower_components/css-modal/build/modal.css" />
-    <!-- endbower -->
-    <link rel="stylesheet" type="text/css" href="/frontend/style.css" media="screen" />
-    <!-- endbuild -->
+    @if (Config::get('app.debug'))
+        <!-- build:css(public) build/style.css -->
+        <!-- bower:css -->
+        <link rel="stylesheet" href="/bower_components/normalize.css/normalize.css" />
+        <link rel="stylesheet" href="/bower_components/remodal/dist/remodal.css" />
+        <!-- endbower -->
+        <link rel="stylesheet" type="text/css" href="/frontend/remodal-default-theme.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/frontend/style.css" media="screen" />
+        <!-- endbuild -->
+    @else
+        <link rel="stylesheet" type="text/css" href="/build/style.css" media="screen" />
+    @endif
 
     <title>{{ $page->title }}</title>
 </head>
@@ -24,10 +29,10 @@
     <div id="bar">
         <h1><a href="/" class="pseudo-anchor">Вислава Шимборская</a>&nbsp;&nbsp;<span class="book-title">Стихотворения</span></h1>
     </div>
-    <div id="main">
-        <div id="leftbar" class="column">
+    <div id="main" class="remodal-bg">
+        <div class="images column">
             <ul id="navigation">
-                <li><a href="#content" class="show-content-link">Содержание</a></li>
+                <li><a href="#content-table" class="show-content-link">Содержание</a></li>
                 <li><a href="/author">Об авторе</a></li>
             </ul>
             <div class="images-bar">
@@ -50,15 +55,18 @@
                     @endif
                     <?php $pageNumber++ ?>
                 @endforeach
-                <li class="last"><a href="#" class="show-content-link">Содержание</a><span class="shortkey">(ctrl + ↑)</span></li>
+                <li class="last"><a href="#content-table" class="show-content-link">Содержание</a><span class="shortkey">(ctrl + ↑)</span></li>
             </ul>
         </div>
 
-        <div class="notabene column">
+        <div class="notes column">
             @yield('notes')
         </div>
 
-        <div id="content" title="Содержание">
+        <div id="content" title="Содержание" class="remodal" data-remodal-id="content-table">
+            <div class="modal-header">
+                <button data-remodal-action="close" class="remodal-close"></button>
+            </div>
             <ul id="contents-wrap">
                 <li class="chapter-link-list wide"><a href="/" class="chapter-link">Обложка</a></li>
                 @foreach ($contentPages as $contentPage)
@@ -84,11 +92,21 @@
     &copy; 2009 Студия «Гриб-дождевик»
 </div>
 
+@if (Config::get('app.debug'))
+<!-- build:js(public) build/script.js -->
 <!-- bower:js -->
-<script src="/bower_components/css-modal/modal.js"></script>
+<script src="/bower_components/jquery/dist/jquery.js"></script>
+<script src="/bower_components/jquery.hotkeys/jquery.hotkeys.js"></script>
+<script src="/bower_components/remodal/dist/remodal.js"></script>
+<script src="/bower_components/hammer.js/hammer.js"></script>
 <!-- endbower -->
 
-<script type="text/javascript" src="/frontend/vendor.js"></script>
 <script type="text/javascript" src="/frontend/script.js"></script>
+<!-- endbuild -->
+@else
+
+<script src="/build/script.js"></script>
+@endif
+
 </body>
 </html>
